32e4c70f0919e72bc1fd53c36fd11f23f1dfb7be
Clément COLIN <clement.colin69@gmail.com>
2022-08-29 15:41:08 +0200

    update ud-viz

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -7,11 +7,7 @@ import { MultiMediaObject } from './MultiMediaObject';
 const app = new udviz.Templates.AllWidget();
 
 app.start('../assets/config/config.json').then((config) => {
-  app.addBaseMapLayer();
-
-  app.addElevationLayer();
-
-  app.setupAndAdd3DTilesLayers();
+  
 
   ////// REQUEST SERVICE
   const requestService = new udviz.Components.RequestService();
@@ -24,104 +20,16 @@ app.start('../assets/config/config.json').then((config) => {
   const help = new udviz.Widgets.Extensions.HelpWindow(config.helpWindow);
   app.addModuleView('help', help);
 
-  ////// AUTHENTICATION MODULE
-  const authenticationService =
-    new udviz.Widgets.Extensions.AuthenticationService(
-      requestService,
-      app.config
-    );
-
-  const authenticationView = new udviz.Widgets.Extensions.AuthenticationView(
-    authenticationService
-  );
-  app.addModuleView('authentication', authenticationView, {
-    type: udviz.Templates.AllWidget.AUTHENTICATION_MODULE,
-  });
-
-  ////// DOCUMENTS MODULE
-  let documentModule = new udviz.Widgets.DocumentModule(
-    requestService,
-    app.config
-  );
-  app.addModuleView('documents', documentModule.view);
-
-  ////// DOCUMENTS VISUALIZER EXTENSION (to orient the document)
-  const imageOrienter = new udviz.Widgets.DocumentVisualizerWindow(
-    documentModule,
-    app.view,
-    app.controls
-  );
-
-  ////// CONTRIBUTE EXTENSION
-  new udviz.Widgets.Extensions.ContributeModule(
-    documentModule,
-    imageOrienter,
-    requestService,
-    app.view,
-    app.controls,
-    app.config
-  );
-
-  ////// VALIDATION EXTENSION
-  new udviz.Widgets.Extensions.DocumentValidationModule(
-    documentModule,
-    requestService,
-    app.config
-  );
-
-  ////// DOCUMENT COMMENTS
-  new udviz.Widgets.Extensions.DocumentCommentsModule(
-    documentModule,
-    requestService,
-    app.config
-  );
-
-  ////// GUIDED TOURS MODULE
-  const guidedtour = new udviz.Widgets.GuidedTourController(
-    documentModule,
-    requestService,
-    app.config
-  );
-  app.addModuleView('guidedTour', guidedtour, {
-    name: 'Guided Tours',
-  });
-
-  ////// GEOCODING EXTENSION
-  const geocodingService = new udviz.Widgets.Extensions.GeocodingService(
-    requestService,
-    app.extent,
-    app.config
-  );
-  const geocodingView = new udviz.Widgets.Extensions.GeocodingView(
-    geocodingService,
-    app.controls,
-    app.view
-  );
-  app.addModuleView('geocoding', geocodingView, {
-    binding: 's',
-    name: 'Address Search',
-  });
-
   ////// CITY OBJECTS MODULE
   let cityObjectModule = new udviz.Widgets.CityObjectModule(
-    app.layerManager,
+    app.view3D.layerManager,
     app.config
   );
   app.addModuleView('cityObjects', cityObjectModule.view);
 
-  ////// LINKS MODULE
-  new udviz.Widgets.LinkModule(
-    documentModule,
-    cityObjectModule,
-    requestService,
-    app.view,
-    app.controls,
-    app.config
-  );
-
   ////// 3DTILES DEBUG
-  const debug3dTilesWindow = new udviz.Widgets.Extensions.Debug3DTilesWindow(
-    app.layerManager
+  const debug3dTilesWindow = new udviz.Widgets.Debug3DTilesWindow(
+    app.view3D.layerManager
   );
   app.addModuleView('3dtilesDebug', debug3dTilesWindow, {
     name: '3DTiles Debug',
@@ -129,8 +37,7 @@ app.start('../assets/config/config.json').then((config) => {
 
   ////// CAMERA POSITIONER
   const cameraPosition = new udviz.Widgets.CameraPositionerView(
-    app.view,
-    app.controls
+    app.view3D.getItownsView()
   );
   app.addModuleView('cameraPositioner', cameraPosition);
 
