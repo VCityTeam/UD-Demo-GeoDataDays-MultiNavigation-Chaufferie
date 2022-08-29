a2502cfe1ad6d16dfdd1079341077239d2baf769
CorentinLIRIS <corentin.chignin@hotmail.fr>
2022-08-29 15:48:47 +0200

    Indent fix

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -56,33 +56,34 @@ app.start('../assets/config/config.json').then((config) => {
       app.controls
     );
 
-  ////// CITY OBJECTS MODULE
-  let cityObjectModule = new udviz.Widgets.CityObjectModule(
-    app.view3D.layerManager,
-    app.config
-  );
-  app.addModuleView('cityObjects', cityObjectModule.view);
+    ////// CITY OBJECTS MODULE
+    let cityObjectModule = new udviz.Widgets.CityObjectModule(
+      app.view3D.layerManager,
+      app.config
+    );
+    app.addModuleView('cityObjects', cityObjectModule.view);
 
-  ////// 3DTILES DEBUG
-  const debug3dTilesWindow = new udviz.Widgets.Debug3DTilesWindow(
-    app.view3D.layerManager
-  );
-  app.addModuleView('3dtilesDebug', debug3dTilesWindow, {
-    name: '3DTiles Debug',
+    ////// 3DTILES DEBUG
+    const debug3dTilesWindow = new udviz.Widgets.Debug3DTilesWindow(
+      app.view3D.layerManager
+    );
+    app.addModuleView('3dtilesDebug', debug3dTilesWindow, {
+      name: '3DTiles Debug',
+    });
+
+    ////// CAMERA POSITIONER
+    const cameraPosition = new udviz.Widgets.CameraPositionerView(
+      app.view3D.getItownsView()
+    );
+    app.addModuleView('cameraPositioner', cameraPosition);
+
+    ////// LAYER CHOICE MODULE
+    const layerChoice = new udviz.Widgets.LayerChoice(app.layerManager);
+    app.addModuleView('layerChoice', layerChoice);
+
+    const inputManager = new udviz.Components.InputManager();
+    ///// SLIDESHOW MODULE
+    const slideShow = new udviz.Widgets.SlideShow(app, inputManager);
+    app.addModuleView('slideShow', slideShow);
   });
-
-  ////// CAMERA POSITIONER
-  const cameraPosition = new udviz.Widgets.CameraPositionerView(
-    app.view3D.getItownsView()
-  );
-  app.addModuleView('cameraPositioner', cameraPosition);
-
-  ////// LAYER CHOICE MODULE
-  const layerChoice = new udviz.Widgets.LayerChoice(app.layerManager);
-  app.addModuleView('layerChoice', layerChoice);
-
-  const inputManager = new udviz.Components.InputManager();
-  ///// SLIDESHOW MODULE
-  const slideShow = new udviz.Widgets.SlideShow(app, inputManager);
-  app.addModuleView('slideShow', slideShow);
 });
